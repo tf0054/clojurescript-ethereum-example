7b618e45d51ae8d0bc5e7aaed58e40508c6f1e0e
Takeshi Nakano <tf0054@gmail.com>
2016-11-28 18:30:09 +0100

    Linted project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
                  [cljs-react-material-ui "0.2.22"]
                  [cljs-web3 "0.16.0-0"]
                  [cljsjs/bignumber "2.1.4-1"]
-                 [cljsjs/react-flexbox-grid "0.10.2-1" :exclusions [cljsjs/react]]
+                 [cljsjs/react-flexbox-grid "0.10.2-1"
+                  :exclusions [cljsjs/react]]
                  [com.andrewmcveigh/cljs-time "0.4.0"]
                  [compojure "1.6.0-beta1"]
                  [day8.re-frame/http-fx "0.0.4"]
@@ -16,13 +17,14 @@
                  [org.clojure/clojurescript "1.9.229"]
                  [print-foo-cljs "2.0.3"]
                  [re-frame "0.8.0"]
-                 [reagent "0.6.0" :exclusions [cljsjs/react]]
+                 [reagent "0.6.0"
+                  :exclusions [cljsjs/react]]
                  [ring.middleware.logger "0.5.0"]
                  [ring/ring-core "1.6.0-beta5"]
                  [ring/ring-defaults "0.3.0-beta1"]
                  [ring/ring-devel "1.6.0-beta5"]
-[io.github.theasp/simple-encryption "0.1.0"]
-]
+                 [io.github.theasp/simple-encryption "0.1.0"]
+                 ]
 
   :plugins [[lein-auto "0.1.2"]
             [lein-cljsbuild "1.1.4"]
@@ -36,22 +38,22 @@
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
-  :figwheel {:css-dirs ["resources/public/css"]
-             :server-port 8080
+  :figwheel {:css-dirs     ["resources/public/css"]
+             :server-port  8080
              :ring-handler user/http-handler}
 
   :auto {"compile-solidity" {:file-pattern #"\.(sol)$"
-                             :paths ["resources/public/contracts/src"]}}
+                             :paths        ["resources/public/contracts/src"]}}
 
   :aliases {"compile-solidity" ["shell" "./compile-solidity.sh"]}
 
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
   :less {:source-paths ["resources/public/less"]
-         :target-path "resources/public/css"
-         :target-dir "resources/public/css"
-         :source-map true
-         :compression true}
+         :target-path  "resources/public/css"
+         :target-dir   "resources/public/css"
+         :source-map   true
+         :compression  true}
 
   :uberjar-name "clojurescript-ethereum-example.jar"
 
@@ -61,29 +63,29 @@
                    [com.cemerick/piggieback "0.2.1"]
                    [figwheel-sidecar "0.5.8"]
                    [org.clojure/tools.nrepl "0.2.11"]]
-    :plugins [[lein-figwheel "0.5.8"]]
+    :plugins      [[lein-figwheel "0.5.8"]]
     :source-paths ["env/dev"]
-    :cljsbuild {:builds [{:id "dev"
-                          :source-paths ["src/cljs"]
-                          :figwheel {:on-jsload "clojurescript-ethereum-example.core/mount-root"}
-                          :compiler {:main clojurescript-ethereum-example.core
-                                     :output-to "resources/public/js/compiled/app.js"
-                                     :output-dir "resources/public/js/compiled/out"
-                                     :asset-path "./js/compiled/out"
-                                     :source-map-timestamp true
-                                     :optimizations :none
-                                     :closure-defines {goog.DEBUG true}
-                                     :preloads [print.foo.preloads.devtools]}}]}}
+    :cljsbuild    {:builds [{:id           "dev"
+                             :source-paths ["src/cljs"]
+                             :figwheel     {:on-jsload "clojurescript-ethereum-example.core/mount-root"}
+                             :compiler     {:main                 clojurescript-ethereum-example.core
+                                            :output-to            "resources/public/js/compiled/app.js"
+                                            :output-dir           "resources/public/js/compiled/out"
+                                            :asset-path           "./js/compiled/out"
+                                            :source-map-timestamp true
+                                            :optimizations        :none
+                                            :closure-defines      {goog.DEBUG true}
+                                            :preloads             [print.foo.preloads.devtools]}}]}}
 
-   :uberjar {:hooks [leiningen.cljsbuild]
+   :uberjar {:hooks       [leiningen.cljsbuild]
              :omit-source true
-             :aot :all
-             :main emojillionaire.core
-             :cljsbuild {:builds {:app {:id "uberjar"
-                                        :source-paths ["src/cljs"]
-                                        :compiler {:main clojurescript-ethereum-example.core
-                                                   :output-to "resources/public/js/compiled/app.js"
-                                                   :optimizations :advanced
-                                                   :closure-defines {goog.DEBUG false}
-                                                   :pretty-print true
-                                                   :pseudo-names true}}}}}})
+             :aot         :all
+             :main        emojillionaire.core
+             :cljsbuild   {:builds {:app {:id           "uberjar"
+                                          :source-paths ["src/cljs"]
+                                          :compiler     {:main            clojurescript-ethereum-example.core
+                                                         :output-to       "resources/public/js/compiled/app.js"
+                                                         :optimizations   :advanced
+                                                         :closure-defines {goog.DEBUG false}
+                                                         :pretty-print    true
+                                                         :pseudo-names    true}}}}}})
